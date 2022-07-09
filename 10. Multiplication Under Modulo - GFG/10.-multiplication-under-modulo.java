1ed330a2f112a323c012d989dd77ba42085fc212
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-07-08 21:26:26 -0400

    Added solution - LeetHub

---
+++ 10. Multiplication Under Modulo - GFG/10.-multiplication-under-modulo.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //taking testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            
+                        
+            //taking input a and b
+            long a = Long.parseLong(str[0]);
+            long b = Long.parseLong(str[1]);
+            
+
+            //calling multiplicationUnderModulo() function
+            System.out.println(new Solution().multiplicationUnderModulo(a, b));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static long multiplicationUnderModulo(long a, long b)
+    {
+        // add your code here
+        long mod = (long)((Math.pow(10,9) + 7));
+        long x = a%mod;
+        long y = b%mod;
+        return (x*y)%mod;
+    }
+}
+
+

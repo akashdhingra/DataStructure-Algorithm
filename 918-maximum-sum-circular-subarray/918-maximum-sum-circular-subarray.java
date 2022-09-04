5f3ab1e0aa58e8427e157f5c36df373adc45decb
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-09-03 20:09:25 -0400

    Time: 9 ms (19.93%), Space: 64.7 MB (28.64%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,37 +1,56 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-        int n = nums.length;
-        int max_count = nums[0];
-        int sum = nums[0];
-        int min_count = nums[0];
-        int min_sum = nums[0];
+    
+    public int normal_max_sum(int [] arr)
+    {
+        int n = arr.length;
+        int res = arr[0];
+        int max_ending = arr[0];
         for(int i=1;i<n;i++)
         {
-           sum = Math.max(sum + nums[i], nums[i]);
-           max_count = Math.max(sum,max_count); 
+           max_ending = Math.max(max_ending + arr[i], arr[i]);
+           res = Math.max(max_ending,res); 
         }
         
-        if(max_count < 0)
-            return max_count;
+        return res;
+    }
+    
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length;
+        int max_normal_sum = normal_max_sum(nums);
+        if(max_normal_sum < 0)
+            return max_normal_sum;
         
-        for(int j=1;j<n;j++)
+        int arr_sum = 0;
+        
+        for(int j=0;j<n;j++)
         {
-           min_sum = Math.min(min_sum + nums[j], nums[j]);
-           min_count = Math.min(min_sum,min_count); 
+            arr_sum = arr_sum + nums[j];
+            nums[j] = -nums[j];
         }
+        
+        int arr_circular_sum = normal_max_sum(nums) + arr_sum;
+        
+        return Math.max(max_normal_sum, arr_circular_sum);
+        
+        
+//         for(int j=1;j<n;j++)
+//         {
+//            min_sum = Math.min(min_sum + nums[j], nums[j]);
+//            min_count = Math.min(min_sum,min_count); 
+//         }
 
-        int initial_sum = 0;
+//         int initial_sum = 0;
         
-        for(int k=0;k<n;k++)
-        {
-           initial_sum = initial_sum + nums[k]; 
-        }
+//         for(int k=0;k<n;k++)
+//         {
+//            initial_sum = initial_sum + nums[k]; 
+//         }
         
-        int final_sum = initial_sum - min_count;
+//         int final_sum = initial_sum - min_count;
         
-        int final_result = Math.max(final_sum,max_count);
+//         int final_result = Math.max(final_sum,max_count);
         
-        return final_result;
+//        return final_result;
         
     }
 }

1e384c1f9830439a2f8ce7942cdda3c2fa54e6f8
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-09-17 17:09:23 -0400

    Time: 5 ms (87.97%), Space: 76.4 MB (29.27%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,8 +1,15 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //created the boolean as length of nums
+        // by default all are of false value
         boolean b[] = new boolean[nums.length];
         for(int i=0;i<nums.length;i++)
         {
+            // set of the value of indexes of b
+            // according to value of nums as true
+            // means set visited as true
+            // if value repeats again then it will returned as it is already true.
+            
             if(b[nums[i]])
                 return nums[i];
             b[nums[i]] = true;

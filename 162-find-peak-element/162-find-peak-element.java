81029f9091b53919afc4b44e34f801fa0b416b04
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-09-17 13:00:44 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (68.90%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,14 +1,26 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        //applied binary search
         int n = nums.length;
         int low = 0;
         int high = n-1;
         while(low<=high)
         {
             int mid = (low+high)/2;
+            
+            // if mid is starting or ending then return mid
+            // or if mid is greater than previous and next element
+            // then return mid
+            
             if((mid ==0 || nums[mid-1] <=nums[mid]) && 
                (mid==n-1 || nums[mid+1] <=nums[mid]))
                 return mid;
+            
+            // if mid is less than previous element then
+            // definately peak element is to the left of mid
+            // as for the element to be peak, both next and previous
+            // element must be smaller.
+            
             if(mid >0 && nums[mid-1] >= nums[mid])
                 high = mid-1;
             else

b563eaa7f8682856bab4d973a9a75380cf02d34b
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-09-17 10:41:24 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (94.47%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int search(int[] arr, int target) {
+        //Applied binary search
         int n = arr.length;
         int low = 0;
         int high = n-1;
@@ -8,7 +9,11 @@ class Solution {
             int mid = (low+high)/2;
             if(arr[mid] == target)
                 return mid;
-            
+            // check if the low is less than mid, if yes
+            // it means that array is sorted from low to mid.
+            // then see if target is greater than or equal to low
+            // and target is lower than mid which means that target
+            // is in left half.
             if(arr[low] <= arr[mid])
             {
                 if(target >= arr[low] && target < arr[mid])
@@ -16,6 +21,7 @@ class Solution {
                 else
                     low = mid+1;
             }
+            //if not then check if target is greater than mid and less than last                 // element then it means element is in right half.
             else
             {
                 if(target >arr[mid] && target <= arr[high])

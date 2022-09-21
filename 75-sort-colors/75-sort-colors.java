c719aec0461082447e693b630b45b410315c2db4
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-09-21 11:17:06 -0400

    Time: 10 ms (5.09%), Space: 43 MB (8.47%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,11 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
         // This is simple bubble sort
+        // for optimization we can run the inner loop
+        // until n-i-1, because after each the last element gets
+        // sorted
         int n = nums.length;
         int temp = 0;
         for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-1;j++)
+            for(int j=0;j<n-i-1;j++)
             {
                 if(nums[j] > nums[j+1])
                 {

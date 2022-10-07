a4f9d64d5dc7fd8b55140a2d07b9d97dcb4db2e1
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-06 22:28:51 -0400

    Time: 4 ms (56.00%), Space: 42.6 MB (11.67%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        // getOrDefault is the method of hashmap that is used to return
+        // the value of value at mentioned key getOrDefault(k,df),
+        // if key is not found then df, which will be a default value set
+        // by user will be returned.
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        for(int i:arr)
+            hmap.put(i,hmap.getOrDefault(i,0)+1);
+        
+        //checks if the value of the key is equal to the size of the created key
+        // if every value is unique then set will create the hashset of value equal to
+        // size of hashmap
+        // return will be true if hashset is equal to hashmap size.
+        // otherwise false.
+        HashSet<Integer> hs = new HashSet<>(hmap.values());
+        return hs.size() == hmap.size();
+    }
+}

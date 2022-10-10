7a4e287dc6a7842ce52b733f12a35e5556e16a4f
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-10 09:12:46 -0400

    Time: 0 ms (100.00%), Space: 42 MB (71.18%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,35 +14,36 @@
  * }
  */
 // Solution 1 --> When using inorder in separate function
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         List<Integer> res = new ArrayList<>();
-//         inorder(res,root);
-//         return res;
-//     }
-    
-//     public void inorder(List<Integer> res, TreeNode root){
-//         if(root == null) return;
-//         inorder(res, root.left);
-//         res.add(root.val);
-//         inorder(res, root.right);
-//     }
-// }
-
-// Solution 2
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null)
-            return new ArrayList<Integer>();
-        
-        List<Integer> res = inorderTraversal(root.left);
-        res.add(root.val);
-        res.addAll(inorderTraversal(root.right));
-        
+        List<Integer> res = new ArrayList<>();
+        inorder(res,root);
         return res;
-        
+    }
+    
+    public void inorder(List<Integer> res, TreeNode root){
+        if(root == null) return;
+        inorder(res, root.left);
+        res.add(root.val);
+        inorder(res, root.right);
     }
 }
+
+// Solution 2 --> Below is the code as one function [No need to create separate inorder function]
+// It works because we are storing the result of every recursive call
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if (root == null)
+//             return new ArrayList<Integer>();
+        
+//         List<Integer> res = inorderTraversal(root.left);
+//         res.add(root.val);
+//         res.addAll(inorderTraversal(root.right));
+        
+//         return res;
+        
+//     }
+// }
 
 
 // Below solution does not works because after every recursion call

eaee4cb24b693ff8f1550b21644db2fb86e1cb6d
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-10 09:11:02 -0400

    Time: 0 ms (100.00%), Space: 43.1 MB (7.76%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,24 +14,40 @@
  * }
  */
 // Solution 1 --> When using inorder in separate function
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         inorder(res,root);
+//         return res;
+//     }
+    
+//     public void inorder(List<Integer> res, TreeNode root){
+//         if(root == null) return;
+//         inorder(res, root.left);
+//         res.add(root.val);
+//         inorder(res, root.right);
+//     }
+// }
+
+// Solution 2
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        inorder(res,root);
-        return res;
-    }
-    
-    public void inorder(List<Integer> res, TreeNode root){
-        if(root == null) return;
-        inorder(res, root.left);
+        if (root == null)
+            return new ArrayList<Integer>();
+        
+        List<Integer> res = inorderTraversal(root.left);
         res.add(root.val);
-        inorder(res, root.right);
+        res.addAll(inorderTraversal(root.right));
+        
+        return res;
+        
     }
 }
 
 
 // Below solution does not works because after every recursion call
 // new res is created and the result of recursion call is not stored any where
+// so to make it work, see solution 2
 // class Solution {
 //     public List<Integer> inorderTraversal(TreeNode root) {
 //         List<Integer> res = new ArrayList<>();

5063bfecba7870b50b812624095eb98c9d749413
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-11 15:22:42 -0400

    Time: 7 ms (42.85%), Space: 38.9 MB (95.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,23 +1,37 @@
+// Method 2 - Recursion
 class Solution {
     public int fib(int n) {
-        int a=0;
-        int b=1;
         if(n==0)
             return 0;
         if(n==1 || n==2)
             return 1;
-        else
-        {
-            
-            for(int i=1;i<n;i++)
-            {
-                int temp = b;
-                b = a+b;
-                a = temp;
-            }
-        }
-        return b;
         
-        
+        return fib(n-1) + fib(n-2);
     }
 }
+
+
+
+// Method 1 - Naive Method
+// class Solution {
+//     public int fib(int n) {
+//         int a=0;
+//         int b=1;
+//         if(n==0)
+//             return 0;
+//         if(n==1 || n==2)
+//             return 1;
+//         else
+//         {
+            
+//             for(int i=1;i<n;i++)
+//             {
+//                 int temp = b;
+//                 b = a+b;
+//                 a = temp;
+//             }
+//         }
+//         return b;
+        
+//     }
+// }

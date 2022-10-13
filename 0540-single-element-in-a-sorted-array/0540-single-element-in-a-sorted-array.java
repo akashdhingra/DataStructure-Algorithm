c5127d42bb1efffd209e7653288813e62db455c5
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-13 15:41:07 -0400

    Time: 3 ms (15.86%), Space: 60.8 MB (26.46%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,19 +3,34 @@
 // positions of (1,3,5..)  [Odd positions] will be first occurance
 // So by following this logic we do the binary search.
 
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int l = 0;
+//         int h = nums.length-1;
+//          while(l<h)
+//          {
+//              int mid = l + (h-l)/2;
+//              if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 != 0 && nums[mid] == nums[mid-1]))
+//                  l = mid+1;
+//              else
+//                  h = mid;
+//          }
+        
+//         return nums[l];
+//     }
+// }
+
+// Solution 2 - xor of same numbers will be 0 [a ^ a = 0]
+// and XOR with 0 is number itself [a ^ 0 = 0]
+// XOR of array is the solution
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int l = 0;
-        int h = nums.length-1;
-         while(l<h)
-         {
-             int mid = l + (h-l)/2;
-             if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 != 0 && nums[mid] == nums[mid-1]))
-                 l = mid+1;
-             else
-                 h = mid;
-         }
-        
-        return nums[l];
+        int XOR = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            XOR = XOR ^ nums[i];
+        }
+        return XOR;
     }
 }

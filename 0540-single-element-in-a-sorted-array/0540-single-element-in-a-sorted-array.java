658ddc15bdfff2d94bfb825375a1b9cf4197ba73
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-10-13 15:42:43 -0400

    Time: 0 ms (100.00%), Space: 47.8 MB (98.26%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,35 +2,35 @@
 // is equal to (1,3,5..)  [Odd positions] but if any single value appears then  
 // positions of (1,3,5..)  [Odd positions] will be first occurance
 // So by following this logic we do the binary search.
-
-// class Solution {
-//     public int singleNonDuplicate(int[] nums) {
-//         int l = 0;
-//         int h = nums.length-1;
-//          while(l<h)
-//          {
-//              int mid = l + (h-l)/2;
-//              if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 != 0 && nums[mid] == nums[mid-1]))
-//                  l = mid+1;
-//              else
-//                  h = mid;
-//          }
+// Complexity - O(logn)
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int h = nums.length-1;
+         while(l<h)
+         {
+             int mid = l + (h-l)/2;
+             if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 != 0 && nums[mid] == nums[mid-1]))
+                 l = mid+1;
+             else
+                 h = mid;
+         }
         
-//         return nums[l];
-//     }
-// }
+        return nums[l];
+    }
+}
 
 // Solution 2 - xor of same numbers will be 0 [a ^ a = 0]
 // and XOR with 0 is number itself [a ^ 0 = 0]
-// XOR of array is the solution
+// XOR of array is the solution - Complexity [O(n)]
 
-class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int XOR = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            XOR = XOR ^ nums[i];
-        }
-        return XOR;
-    }
-}
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int XOR = 0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             XOR = XOR ^ nums[i];
+//         }
+//         return XOR;
+//     }
+// }

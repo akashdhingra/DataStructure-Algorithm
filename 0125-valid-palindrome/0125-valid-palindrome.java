6232d2d7c30cbd494dcbe6c14aafb7a06cf4a2d2
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-11-26 09:54:11 -0500

    Time: 1713 ms (5.00%), Space: 643.8 MB (5.01%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String user_string = "";
+        String reverse_string = "";
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>='a' && s.charAt(i) <='z' || s.charAt(i)>='A' && s.charAt(i) <='Z'  || s.charAt(i)>= '0' && s.charAt(i) <='9')
+            {
+                user_string = user_string + Character.toLowerCase(s.charAt(i));
+            }
+            
+        }
+        
+        for(int j=user_string.length()-1;j>=0;j--){
+            reverse_string = reverse_string + user_string.charAt(j);
+        }
+        
+        if(user_string.equals(reverse_string))
+            return true;
+        return false;
+    }
+    
+}

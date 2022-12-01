0c191afd9acb2bf0e6f485042cfd22f05cc3cda7
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-12-01 18:14:05 -0500

    Time: 5 ms (78.55%), Space: 40.7 MB (86.18%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,16 +1,16 @@
+// Solution using Set
 class Solution {
     public boolean halvesAreAlike(String s) {
         int count = 0;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
         String s1 = s.toLowerCase().substring(0,s.length()/2);
         String s2 = s.toLowerCase().substring(s.length()/2,s.length());
         
         for(int i=0;i<s.length()/2;i++){
-            if(s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i'||
-              s1.charAt(i) == 'o' || s1.charAt(i) == 'u')
+            if(vowels.contains(s1.charAt(i)))
                 count++;
             
-            if(s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i'||
-              s2.charAt(i) == 'o' || s2.charAt(i) == 'u')
+            if(vowels.contains(s2.charAt(i)))
                 count--;
         }
         
@@ -19,6 +19,30 @@ class Solution {
         return false;
     }
 }
+
+// Solution 2
+
+// class Solution {
+//     public boolean halvesAreAlike(String s) {
+//         int count = 0;
+//         String s1 = s.toLowerCase().substring(0,s.length()/2);
+//         String s2 = s.toLowerCase().substring(s.length()/2,s.length());
+        
+//         for(int i=0;i<s.length()/2;i++){
+//             if(s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i'||
+//               s1.charAt(i) == 'o' || s1.charAt(i) == 'u')
+//                 count++;
+            
+//             if(s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i'||
+//               s2.charAt(i) == 'o' || s2.charAt(i) == 'u')
+//                 count--;
+//         }
+        
+//         if(count == 0)
+//             return true;
+//         return false;
+//     }
+// }
 
 // Solution 1
 // class Solution {

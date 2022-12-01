b2f0034fc931fed883558161be6c433244302e7d
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2022-12-01 18:10:29 -0500

    Time: 21 ms (10.08%), Space: 42.6 MB (28.08%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,22 +1,45 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int count1 = 0;
-        int count2 = 0;
+        int count = 0;
         String s1 = s.toLowerCase().substring(0,s.length()/2);
         String s2 = s.toLowerCase().substring(s.length()/2,s.length());
         
         for(int i=0;i<s.length()/2;i++){
             if(s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i'||
               s1.charAt(i) == 'o' || s1.charAt(i) == 'u')
-                count1++;
+                count++;
             
             if(s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i'||
               s2.charAt(i) == 'o' || s2.charAt(i) == 'u')
-                count2++;
+                count--;
         }
         
-        if(count1==count2)
+        if(count == 0)
             return true;
         return false;
     }
 }
+
+// Solution 1
+// class Solution {
+//     public boolean halvesAreAlike(String s) {
+//         int count1 = 0;
+//         int count2 = 0;
+//         String s1 = s.toLowerCase().substring(0,s.length()/2);
+//         String s2 = s.toLowerCase().substring(s.length()/2,s.length());
+        
+//         for(int i=0;i<s.length()/2;i++){
+//             if(s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i'||
+//               s1.charAt(i) == 'o' || s1.charAt(i) == 'u')
+//                 count1++;
+            
+//             if(s2.charAt(i) == 'a' || s2.charAt(i) == 'e' || s2.charAt(i) == 'i'||
+//               s2.charAt(i) == 'o' || s2.charAt(i) == 'u')
+//                 count2++;
+//         }
+        
+//         if(count1==count2)
+//             return true;
+//         return false;
+//     }
+// }

b6955cd5df1de95df5ece4cc9107bd835e5d1905
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2023-01-29 21:50:55 -0500

    Added solution - LeetHub

--- Palindromic Array - GFG/palindromic-array.java
+++ Palindromic Array - GFG/palindromic-array.java
@@ -19,41 +19,65 @@ class PalindromicArray{
 
 
 /*Complete the Function below*/
+// Method 1
 class GfG
 {
     
-    public static boolean isPalindrome(int a){
-        int num = 0;
-        int value = a;
-        if(a==0){
-            return true;
-        }
-        
-        while(a>0){
-            num = (num*10) + a%10;
-            a = a/10;
-        }
-        
-        if(num == value){
-            return true;
-        }
-        return false;
-    }
-    
 	public static int palinArray(int[] a, int n)
-           {
-                  //add code here.
-                  for(int i=0;i<n;i++)
-                  {
-                      if(!isPalindrome(a[i])){
-                          return 0;
-                      }
+       {
+              //add code here.
+              for(int i=0;i<n;i++)
+              {
+                  int num = a[i];
+                  int original_num = num;
+                  int reverse = 0;
+                  while(num!=0){
+                      reverse = (reverse*10) + num%10;
+                      num = num/10;
                   }
-                  return 1;
-           }
+                  
+                  if(original_num!=reverse)
+                    return 0;
+              }
+              return 1;
+       }
 }
 
+// Method 2
 
+// class GfG
+// {
+    
+//     public static boolean isPalindrome(int a){
+//         int num = 0;
+//         int value = a;
+//         if(a==0){
+//             return true;
+//         }
+        
+//         while(a>0){
+//             num = (num*10) + a%10;
+//             a = a/10;
+//         }
+        
+//         if(num == value){
+//             return true;
+//         }
+//         return false;
+//     }
+    
+// 	public static int palinArray(int[] a, int n)
+//           {
+//                   //add code here.
+//                   for(int i=0;i<n;i++)
+//                   {
+//                       if(!isPalindrome(a[i])){
+//                           return 0;
+//                       }
+//                   }
+//                   return 1;
+//           }
+// }
 
 
 

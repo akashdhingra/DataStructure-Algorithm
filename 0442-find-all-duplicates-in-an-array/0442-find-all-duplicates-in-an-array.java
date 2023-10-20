aaa6547d7195f37bf3cd3827d8219089a6003e3b
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2023-10-20 11:16:49 -0400

    Time: 6 ms (64.17%), Space: 53 MB (85.22%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,22 +1,42 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        Map<Integer, Integer> hs = new HashMap<>();
         List<Integer> ls = new ArrayList<>();
         
         for(int i : nums){
-            if(hs.containsKey(i)){
-                hs.put(i,hs.get(i)+1);
+            int index = Math.abs(i) - 1;
+            if(nums[index] < 0){
+                ls.add(Math.abs(i));
             }
             else{
-                hs.put(i,1);
-            }
-        }
-        
-        for(Integer i : hs.keySet()){
-            if(hs.get(i) == 2){
-                ls.add(i);
+                nums[index] = -nums[index];
             }
         }
         return ls;
     }
 }
+        
+        
+        
+        
+        
+        
+//         Below solution is correct but it uses extra space
+//         Map<Integer, Integer> hs = new HashMap<>();
+//         List<Integer> ls = new ArrayList<>();
+        
+//         for(int i : nums){
+//             if(hs.containsKey(i)){
+//                 hs.put(i,hs.get(i)+1);
+//             }
+//             else{
+//                 hs.put(i,1);
+//             }
+//         }
+        
+//         for(Integer i : hs.keySet()){
+//             if(hs.get(i) == 2){
+//                 ls.add(i);
+//             }
+//         }
+//         return ls;
+ 

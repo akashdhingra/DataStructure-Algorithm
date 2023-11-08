02953cc38847a0ea362eaefc472b70948d4707f8
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2023-11-08 13:17:05 -0500

    Added solution - LeetHub

--- Peak element - GFG/peak-element.java
+++ Peak element - GFG/peak-element.java
@@ -51,21 +51,48 @@ class Solution
 	// n: size of array a[]
 	public int peakElement(int[] arr,int n)
     {
-       //add code here.
-       if(n == 1)
-       return 0;
-       
-       if(arr[0] >= arr[1])
-       return 0;
-       
-       for(int i=1;i<n-1;i++){
-           if(arr[i] >= arr[i+1] && arr[i] >= arr[i-1])
-           return i;
-       }
-       
-       if(arr[n-1] > arr[n-2])
-       return n-1;
-       
-       return -1;
+        int left = 0;
+        int right = n-1;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            
+            if(arr[mid] > arr[mid+1]){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+        
     }
+       
 }
+
+// Below solution takes O(n) time
+
+// class Solution
+// {
+// 	// Function to find the peak element
+// 	// arr[]: input array
+// 	// n: size of array a[]
+// 	public int peakElement(int[] arr,int n)
+//     {
+//       //add code here.
+//       if(n == 1)
+//       return 0;
+       
+//       if(arr[0] >= arr[1])
+//       return 0;
+       
+//       for(int i=1;i<n-1;i++){
+//           if(arr[i] >= arr[i+1] && arr[i] >= arr[i-1])
+//           return i;
+//       }
+       
+//       if(arr[n-1] > arr[n-2])
+//       return n-1;
+       
+//       return -1;
+//     }
+// }

1542a7ea9fef5ffbf66ba15299a436713f62f4f1
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2023-11-17 01:35:00 -0500

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.java
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -36,62 +36,114 @@ class GFG {
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
-        //Your code here
-        sort(arr, l , r);
-        return arr[k-1];
-    } 
-    
-    static void sort(int arr[], int l, int r){
-        if(l < r){
-            int mid = l + (r-l)/2;
-            sort(arr, l, mid);
-            sort(arr, mid+1, r);
-            merge(arr, l, mid, r);
-        }
-    }
-    
-    static void merge(int[] arr, int l, int mid, int r){
-        int n1 = mid - l + 1;
-        int n2 = r-mid;
-        
-        int left[] = new int[n1];
-        int right[] = new int[n2];
-        
-        for(int i=0;i<n1;i++){
-            left[i] = arr[l+i];
-        }
-        
-        for(int j=0;j<n2;j++){
-            right[j] = arr[mid+1+j];
-        }
-        
-        int i=0;
-        int j=0;
-        int k=l;
-        
-        while(i<n1 && j<n2){
-            if(left[i] <= right[j]){
-                arr[k] = left[i];
-                i++;
-            }
-            else{
-                arr[k] = right[j];
-                j++;
+        // Your code here
+        if(k > 0 && k <= r-l+1){
+            int partitionIndex = partition(arr, l, r);
+            
+            if(partitionIndex-l == k-1){
+                return arr[partitionIndex];
             }
             
-            k++;
+            if(partitionIndex-l > k-1){
+                return kthSmallest(arr, l, partitionIndex-1,k);
+                
+            }
+            else{
+                return kthSmallest(arr,partitionIndex+1,r,k-partitionIndex + l - 1);
+            }
         }
         
-        while(i<n1){
-            arr[k] = left[i];
-            k++;
-            i++;
+        return Integer.MAX_VALUE;
+       
+    } 
+    
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        
+        int i = low;
+        
+        for(int j=low;j<=high-1;j++){
+            if(arr[j] <= pivot){
+                swap(arr, i, j);
+                i++;
+            }
         }
         
-        while(j<n2){
-            arr[k] = right[j];
-            k++;
-            j++;
-        }
+        swap(arr, i, high);
+        
+        return i;
     }
+    
+    static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
 }
+
+// Below solution is done through sort but it will take unnecessary time to sort the elements.
+
+// class Solution{
+//     public static int kthSmallest(int[] arr, int l, int r, int k) 
+//     { 
+//         //Your code here
+//         sort(arr, l , r);
+//         return arr[k-1];
+//     } 
+    
+//     static void sort(int arr[], int l, int r){
+//         if(l < r){
+//             int mid = l + (r-l)/2;
+//             sort(arr, l, mid);
+//             sort(arr, mid+1, r);
+//             merge(arr, l, mid, r);
+//         }
+//     }
+    
+//     static void merge(int[] arr, int l, int mid, int r){
+//         int n1 = mid - l + 1;
+//         int n2 = r-mid;
+        
+//         int left[] = new int[n1];
+//         int right[] = new int[n2];
+        
+//         for(int i=0;i<n1;i++){
+//             left[i] = arr[l+i];
+//         }
+        
+//         for(int j=0;j<n2;j++){
+//             right[j] = arr[mid+1+j];
+//         }
+        
+//         int i=0;
+//         int j=0;
+//         int k=l;
+        
+//         while(i<n1 && j<n2){
+//             if(left[i] <= right[j]){
+//                 arr[k] = left[i];
+//                 i++;
+//             }
+//             else{
+//                 arr[k] = right[j];
+//                 j++;
+//             }
+            
+//             k++;
+//         }
+        
+//         while(i<n1){
+//             arr[k] = left[i];
+//             k++;
+//             i++;
+//         }
+        
+//         while(j<n2){
+//             arr[k] = right[j];
+//             k++;
+//             j++;
+//         }
+//     }
+// }
+

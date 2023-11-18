09118b778d5b9a80de47a8b4895f6fbbdf9c8b3f
Akash Dhingra <38224187+akashdhingra@users.noreply.github.com>
2023-11-18 13:05:43 -0500

    Added solution - LeetHub

--- Find the Frequency - GFG/find-the-frequency.java
+++ Find the Frequency - GFG/find-the-frequency.java
@@ -58,8 +58,8 @@ class GFG {
 class Solution{
     int findFrequency(int A[], int x){
         int count = 0;
-        for(int i=0;i<A.length;i++){
-            if(A[i] == x){
+        for(int i : A){
+            if(i == x){
                 count++;
             }
         }
@@ -67,3 +67,16 @@ class Solution{
         return count;
     }
 }
+
+// class Solution{
+//     int findFrequency(int A[], int x){
+//         int count = 0;
+//         for(int i=0;i<A.length;i++){
+//             if(A[i] == x){
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
